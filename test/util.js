b3f74a24bb1df225e51355363ae769e0db7f66ce
Zack Urben <zackurben@gmail.com>
2017-08-13 10:46:15 -0500

    Adding daily data polishing.

--- test/util.js
+++ test/util.js
@@ -54,3 +54,53 @@ test(`intraday data polishing works`, () => {
     expect(polished['data'][first]['volume']).toBeDefined();
   });
 });
+
+test(`daily data polishing works`, () => {
+  expect.assertions(38);
+  return alpha.data.daily(`msft`).then(data => {
+    const polished = alpha.util.polish(data);
+    let first;
+
+    expect(data['Meta Data']).toBeDefined();
+    expect(data['Meta Data']['1. Information']).toBeDefined();
+    expect(data['Meta Data']['2. Symbol']).toBeDefined();
+    expect(data['Meta Data']['3. Last Refreshed']).toBeDefined();
+    expect(data['Meta Data']['4. Output Size']).toBeDefined();
+    expect(data['Meta Data']['5. Time Zone']).toBeDefined();
+    expect(data['Time Series (Daily)']).toBeDefined();
+    first = Object.keys(data['Time Series (Daily)'])[0];
+    expect(first).toBeDefined();
+    expect(data['Time Series (Daily)'][first]['1. open']).toBeDefined();
+    expect(data['Time Series (Daily)'][first]['2. high']).toBeDefined();
+    expect(data['Time Series (Daily)'][first]['3. low']).toBeDefined();
+    expect(data['Time Series (Daily)'][first]['4. close']).toBeDefined();
+    expect(data['Time Series (Daily)'][first]['5. volume']).toBeDefined();
+
+    expect(polished['Meta Data']).toBeUndefined();
+    expect(polished['Time Series (Daily)']).toBeUndefined();
+    expect(polished['meta']).toBeDefined();
+    expect(polished['meta']['1. Information']).toBeUndefined();
+    expect(polished['meta']['2. Symbol']).toBeUndefined();
+    expect(polished['meta']['3. Last Refreshed']).toBeUndefined();
+    expect(polished['meta']['4. Output Size']).toBeUndefined();
+    expect(polished['meta']['5. Time Zone']).toBeUndefined();
+    expect(polished['meta']['information']).toBeDefined();
+    expect(polished['meta']['symbol']).toBeDefined();
+    expect(polished['meta']['updated']).toBeDefined();
+    expect(polished['meta']['size']).toBeDefined();
+    expect(polished['meta']['zone']).toBeDefined();
+    expect(polished['data']).toBeDefined();
+    first = Object.keys(polished['data'])[0];
+    expect(first).toBeDefined();
+    expect(polished['data'][first]['1. open']).toBeUndefined();
+    expect(polished['data'][first]['2. high']).toBeUndefined();
+    expect(polished['data'][first]['3. low']).toBeUndefined();
+    expect(polished['data'][first]['4. close']).toBeUndefined();
+    expect(polished['data'][first]['5. volume']).toBeUndefined();
+    expect(polished['data'][first]['open']).toBeDefined();
+    expect(polished['data'][first]['high']).toBeDefined();
+    expect(polished['data'][first]['low']).toBeDefined();
+    expect(polished['data'][first]['close']).toBeDefined();
+    expect(polished['data'][first]['volume']).toBeDefined();
+  });
+});
