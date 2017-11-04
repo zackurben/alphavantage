1c8ea922f40726f3b28a7d72ad642bc48eb92bb5
Zack Urben <zackurben@gmail.com>
2017-11-04 13:54:49 -0500

    Adding a delay to all tests, to ensure they dont exceed the request limit.

--- test/data.js
+++ test/data.js
@@ -2,43 +2,55 @@
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 const alpha = require('../')();
+const delay = require('delay');
+const TIME = 1000;
 
 test(`intraday data works`, () => {
   expect.assertions(2);
-  return alpha.data.intraday(`msft`).then(data => {
-    expect(data['Meta Data']).toBeDefined();
-    expect(data['Time Series (1min)']).toBeDefined();
-  });
+  return delay(TIME)
+    .then(alpha.data.intraday(`msft`))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Time Series (1min)']).toBeDefined();
+    });
 });
 
 test(`daily data works`, () => {
   expect.assertions(2);
-  return alpha.data.daily(`msft`).then(data => {
-    expect(data['Meta Data']).toBeDefined();
-    expect(data['Time Series (Daily)']).toBeDefined();
-  });
+  return delay(TIME)
+    .then(alpha.data.daily(`msft`))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Time Series (Daily)']).toBeDefined();
+    });
 });
 
 test(`adjusted data works`, () => {
   expect.assertions(2);
-  return alpha.data.adjusted(`msft`).then(data => {
-    expect(data['Meta Data']).toBeDefined();
-    expect(data['Time Series (Daily)']).toBeDefined();
-  });
+  return delay(TIME)
+    .then(alpha.data.adjusted(`msft`))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Time Series (Daily)']).toBeDefined();
+    });
 });
 
 test(`weekly data works`, () => {
   expect.assertions(2);
-  return alpha.data.weekly(`msft`).then(data => {
-    expect(data['Meta Data']).toBeDefined();
-    expect(data['Weekly Time Series']).toBeDefined();
-  });
+  return delay(TIME)
+    .then(alpha.data.weekly(`msft`))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Weekly Time Series']).toBeDefined();
+    });
 });
 
 test(`monthly data works`, () => {
   expect.assertions(2);
-  return alpha.data.monthly(`msft`).then(data => {
-    expect(data['Meta Data']).toBeDefined();
-    expect(data['Monthly Time Series']).toBeDefined();
-  });
+  return delay(TIME)
+    .then(alpha.data.monthly(`msft`))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Monthly Time Series']).toBeDefined();
+    });
 });
