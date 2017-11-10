2849239293e46142941e361e3b82e8c9a2530f17
Zack Urben <zackurben@gmail.com>
2017-11-10 17:48:58 -0600

    Adding adx indicator and test.

--- test/technical.js
+++ test/technical.js
@@ -284,12 +284,28 @@ test(`willr data works`, () => {
     .then(data => {
       expect(data['Meta Data']).toBeDefined();
       expect(data['Meta Data']['1: Symbol']).toEqual('msft');
-      expect(data['Meta Data']['2: Indicator']).toEqual('Williams\' %R (WILLR)');
+      expect(data['Meta Data']['2: Indicator']).toEqual("Williams' %R (WILLR)");
       expect(data['Meta Data']['3: Last Refreshed']).toBeDefined();
       expect(data['Meta Data']['4: Interval']).toEqual('daily');
       expect(data['Meta Data']['5: Time Period']).toEqual(60);
       expect(data['Meta Data']['6: Time Zone']).toBeDefined();
       expect(data['Technical Analysis: WILLR']).toBeDefined();
+    });
+});
+
+test.only(`adx data works`, () => {
+  expect.assertions(8);
+  return delay(TIME)
+    .then(() => alpha.technical.adx(`msft`, `daily`, 60))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Meta Data']['1: Symbol']).toEqual('msft');
+      expect(data['Meta Data']['2: Indicator']).toEqual('Average Directional Movement Index (ADX)');
+      expect(data['Meta Data']['3: Last Refreshed']).toBeDefined();
+      expect(data['Meta Data']['4: Interval']).toEqual('daily');
+      expect(data['Meta Data']['5: Time Period']).toEqual(60);
+      expect(data['Meta Data']['6: Time Zone']).toBeDefined();
+      expect(data['Technical Analysis: ADX']).toBeDefined();
     });
 });
 
