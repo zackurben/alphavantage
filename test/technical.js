c37fa14c766cbf36c2ce1d68f46fd057b43319b1
Zack Urben <zackurben@gmail.com>
2017-11-10 17:44:23 -0600

    Adding willr indicator and test.

--- test/technical.js
+++ test/technical.js
@@ -277,6 +277,22 @@ test(`stochrsi data works`, () => {
     });
 });
 
+test(`willr data works`, () => {
+  expect.assertions(8);
+  return delay(TIME)
+    .then(() => alpha.technical.willr(`msft`, `daily`, 60))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Meta Data']['1: Symbol']).toEqual('msft');
+      expect(data['Meta Data']['2: Indicator']).toEqual('Williams\' %R (WILLR)');
+      expect(data['Meta Data']['3: Last Refreshed']).toBeDefined();
+      expect(data['Meta Data']['4: Interval']).toEqual('daily');
+      expect(data['Meta Data']['5: Time Period']).toEqual(60);
+      expect(data['Meta Data']['6: Time Zone']).toBeDefined();
+      expect(data['Technical Analysis: WILLR']).toBeDefined();
+    });
+});
+
 test(`mom data works`, () => {
   expect.assertions(9);
   return delay(TIME)
