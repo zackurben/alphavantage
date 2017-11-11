104464136547ca05de96ab476b7400796f27d2f5
Zack Urben <zackurben@gmail.com>
2017-11-10 18:44:41 -0600

    Adding midprice indicator and test.

--- test/technical.js
+++ test/technical.js
@@ -585,3 +585,19 @@ test(`midpoint data works`, () => {
       expect(data['Technical Analysis: MIDPOINT']).toBeDefined();
     });
 });
+
+test(`midprice data works`, () => {
+  expect.assertions(8);
+  return delay(TIME)
+    .then(() => alpha.technical.midprice(`msft`, `daily`, 60))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Meta Data']['1: Symbol']).toEqual('msft');
+      expect(data['Meta Data']['2: Indicator']).toEqual('Midpoint Price over period (MIDPRICE)');
+      expect(data['Meta Data']['3: Last Refreshed']).toBeDefined();
+      expect(data['Meta Data']['4: Interval']).toEqual('daily');
+      expect(data['Meta Data']['5: Time Period']).toEqual(60);
+      expect(data['Meta Data']['6: Time Zone']).toBeDefined();
+      expect(data['Technical Analysis: MIDPRICE']).toBeDefined();
+    });
+});
