f14407aee31661e4381b90c8cb7b84b1a86cd777
Zack Urben <zackurben@gmail.com>
2018-10-16 14:50:14 -0500

    Fixing forex test and updating timeouts.

--- test/technical.js
+++ test/technical.js
@@ -4,7 +4,7 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 jest.unmock('request-promise-native');
 const alpha = require('../')();
 const delay = require('delay');
-const TIME = 5000;
+const TIME = 1000;
 
 test(`sma data works`, () => {
   expect.assertions(9);
