b474cd0f885b4934ab0b12b8faa0909b09c521c2
Ragnar Paide <ragnar.paide@gmail.com>
2020-05-06 23:45:14 +0300

    Adding forex daily support.

--- test/forex.js
+++ test/forex.js
@@ -8,7 +8,7 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 jest.unmock('cross-fetch');
 const TIME = 1000;
 
-test(`forex data works`, () => {
+test(`forex rate works`, () => {
   expect.assertions(3);
   return delay(TIME)
     .then(() => alpha.forex.rate('btc', 'usd'))
@@ -16,5 +16,16 @@ test(`forex data works`, () => {
       expect(data['Realtime Currency Exchange Rate']).toBeDefined();
       expect(data['Realtime Currency Exchange Rate']['1. From_Currency Code']).toEqual('BTC');
       expect(data['Realtime Currency Exchange Rate']['3. To_Currency Code']).toEqual('USD');
+    });
+});
+
+test(`forex daily works`, () => {
+  expect.assertions(3);
+  return delay(TIME)
+    .then(() => alpha.forex.daily('usd', 'eur'))
+    .then(data => {
+      expect(data['Meta Data']).toBeDefined();
+      expect(data['Meta Data']['2. From Symbol']).toEqual('USD');
+      expect(data['Meta Data']['3. To Symbol']).toEqual('EUR');
     });
 });
